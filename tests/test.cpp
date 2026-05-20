e28c2235fdb2c1e2eabc09fb208ea41858057a4d
boban4 <garik.melikov@bk.ru>
2026-05-20 07:10:31 -0500

    lab05: add tests, mocks, CI, coverage

---
+++ tests/test.cpp
@@ -0,0 +1,56 @@
+#include "Account.h"
+#include "Transaction.h"
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+class AccountMock : public Account {
+public:
+    AccountMock(int id, int balance) : Account(id, balance) {}
+    MOCK_METHOD0(GetBalance, int());
+    MOCK_METHOD1(ChangeBalance, void(int));
+    MOCK_METHOD0(Lock, void());
+    MOCK_METHOD0(Unlock, void());
+};
+
+TEST(Account, SimpleTest) {
+    Account acc(1, 100);
+    EXPECT_EQ(acc.id(), 1);
+    EXPECT_EQ(acc.GetBalance(), 100);
+    EXPECT_THROW(acc.ChangeBalance(50), std::runtime_error);
+    EXPECT_NO_THROW(acc.Lock());
+    acc.ChangeBalance(50);
+    EXPECT_EQ(acc.GetBalance(), 150);
+    EXPECT_THROW(acc.Lock(), std::runtime_error);
+    acc.Unlock();
+    EXPECT_NO_THROW(acc.Lock());
+}
+
+TEST(Transaction, MockTest) {
+    MockAccount from(1, 1000);
+    MockAccount to(2, 500);
+    Transaction tx;
+    tx.set_fee(100);
+
+    EXPECT_CALL(from, Lock()).Times(1);
+    EXPECT_CALL(to, Lock()).Times(1);
+    EXPECT_CALL(from, GetBalance()).WillOnce(testing::Return(1000));
+    EXPECT_CALL(from, ChangeBalance(-500)).Times(1);
+    EXPECT_CALL(to, ChangeBalance(400)).Times(1);
+    EXPECT_CALL(from, Unlock()).Times(1);
+    EXPECT_CALL(to, Unlock()).Times(1);
+
+    EXPECT_TRUE(tx.Make(from, to, 500));
+}
+
+TEST(Transaction, SimpleTest) {
+    Transaction tx;
+    Account from(1, 1000);
+    Account to(2, 500);
+    tx.set_fee(100);
+
+    EXPECT_FALSE(tx.Make(from, to, 500));
+    EXPECT_EQ(from.GetBalance(), 1000);
+    EXPECT_TRUE(tx.Make(from, to, 300));
+    EXPECT_EQ(from.GetBalance(), 700);
+    EXPECT_EQ(to.GetBalance(), 800);
+}
