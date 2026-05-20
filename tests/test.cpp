2ec7929b784a11db50ea915700dca5a29c62e35b
boban4 <garik.melikov@bk.ru>
2026-05-20 16:14:33 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,8 @@
-#include "Account.h"
-#include "Transaction.h"
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include "Account.h"
+#include "Transaction.h"
+#include <stdexcept>
 
 using ::testing::Return;
 using ::testing::_;
@@ -9,57 +10,103 @@ using ::testing::_;
 class MockAccount : public Account {
 public:
     MockAccount(int id, int balance) : Account(id, balance) {}
-    MOCK_METHOD0(GetBalance, int());
-    MOCK_METHOD1(ChangeBalance, void(int));
-    MOCK_METHOD0(Lock, void());
-    MOCK_METHOD0(Unlock, void());
+    MOCK_METHOD(int, GetBalance, (), (const, override));
+    MOCK_METHOD(void, ChangeBalance, (int), (override));
+    MOCK_METHOD(void, Lock, (), (override));
+    MOCK_METHOD(void, Unlock, (), (override));
 };
 
-TEST(Account, SimpleTest) {
-    Account acc(1, 100);
-    EXPECT_EQ(acc.id(), 1);
-    EXPECT_EQ(acc.GetBalance(), 100);
-    EXPECT_THROW(acc.ChangeBalance(50), std::runtime_error);
-    EXPECT_NO_THROW(acc.Lock());
-    acc.ChangeBalance(50);
-    EXPECT_EQ(acc.GetBalance(), 150);
-    EXPECT_THROW(acc.Lock(), std::runtime_error);
-    acc.Unlock();
-    EXPECT_NO_THROW(acc.Lock());
+class MockTransaction : public Transaction {
+public:
+    MOCK_METHOD(void, SaveToDataBase, (Account&, Account&, int), (override));
+};
+
+TEST(AccountTest, Initialization) {
+    Account wallet(10, 500);
+    EXPECT_EQ(wallet.id(), 10);
+    EXPECT_EQ(wallet.GetBalance(), 500);
 }
 
-TEST(Transaction, MockTest) {
-    MockAccount from(1, 1000);
-    MockAccount to(2, 500);
+TEST(AccountTest, LockUnlockOperations) {
+    Account wallet(1, 0);
+    EXPECT_NO_THROW(wallet.Lock());
+    EXPECT_THROW(wallet.Lock(), std::runtime_error);
+    EXPECT_NO_THROW(wallet.Unlock());
+}
+
+TEST(AccountTest, BalanceModification) {
+    Account wallet(2, 100);
+    EXPECT_THROW(wallet.ChangeBalance(20), std::runtime_error);
+    wallet.Lock();
+    wallet.ChangeBalance(30);
+    EXPECT_EQ(wallet.GetBalance(), 130);
+}
+
+TEST(TransactionTest, FeeConfiguration) {
     Transaction tx;
-    tx.set_fee(100);
-
-    {
-        testing::InSequence seq;
-
-        EXPECT_CALL(from, Lock()).Times(1);
-        EXPECT_CALL(to, Lock()).Times(1);
-        
-        EXPECT_CALL(to, ChangeBalance(500)).Times(1);
-        
-        // Откат: Debit не сработал, вызывается ChangeBalance(-500)
-        EXPECT_CALL(to, ChangeBalance(-500)).Times(1);
-        
-        EXPECT_CALL(from, Unlock()).Times(1);
-        EXPECT_CALL(to, Unlock()).Times(1);
-    }
-
-    // Make возвращает false, потому что Debit не сработал
-    EXPECT_FALSE(tx.Make(from, to, 500));
+    EXPECT_EQ(tx.fee(), 1);
+    tx.set_fee(5);
+    EXPECT_EQ(tx.fee(), 5);
 }
 
-TEST(Transaction, SimpleTest) {
+TEST(TransactionTest, ValidationErrors) {
+    MockTransaction tx;
+    MockAccount src(1, 1000), dst(2, 1000);
+
+    EXPECT_THROW(tx.Make(src, src, 200), std::logic_error);
+    EXPECT_THROW(tx.Make(src, dst, -10), std::invalid_argument);
+    EXPECT_THROW(tx.Make(src, dst, 50), std::logic_error);
+
+    tx.set_fee(60);
+    EXPECT_FALSE(tx.Make(src, dst, 100));
+}
+
+TEST(TransactionTest, SuccessfulTransfer) {
+    MockTransaction tx;
+    MockAccount src(1, 5000), dst(2, 5000);
+    tx.set_fee(1);
+
+    EXPECT_CALL(src, Lock());
+    EXPECT_CALL(dst, Lock());
+    EXPECT_CALL(dst, ChangeBalance(100));
+    EXPECT_CALL(dst, GetBalance()).WillOnce(Return(2000));
+    EXPECT_CALL(dst, ChangeBalance(-101));
+    EXPECT_CALL(tx, SaveToDataBase(_, _, 100));
+    EXPECT_CALL(dst, Unlock());
+    EXPECT_CALL(src, Unlock());
+
+    EXPECT_TRUE(tx.Make(src, dst, 100));
+}
+
+TEST(TransactionTest, FailedDebitRollback) {
+    MockTransaction tx;
+    MockAccount src(1, 5000), dst(2, 5000);
+    tx.set_fee(1);
+
+    EXPECT_CALL(src, Lock());
+    EXPECT_CALL(dst, Lock());
+    EXPECT_CALL(dst, ChangeBalance(100));
+    EXPECT_CALL(dst, GetBalance()).WillOnce(Return(50));
+    EXPECT_CALL(dst, ChangeBalance(-100));
+    EXPECT_CALL(tx, SaveToDataBase(_, _, 100));
+    EXPECT_CALL(dst, Unlock());
+    EXPECT_CALL(src, Unlock());
+
+    EXPECT_FALSE(tx.Make(src, dst, 100));
+}
+
+TEST(TransactionTest, DestructorCoverage) {
+    Transaction* tx = new Transaction();
+    delete tx;
+    
+    Account* acc = new Account(1, 100);
+    delete acc;
+}
+
+TEST(TransactionTest, RealExecutionPath) {
     Transaction tx;
     Account from(1, 1000);
-    Account to(2, 500);
-    tx.set_fee(100);
+    Account to(2, 1000);
 
-    EXPECT_TRUE(tx.Make(from, to, 500));
-    EXPECT_EQ(from.GetBalance(), 1000);
-    EXPECT_EQ(to.GetBalance(), 400);
+    EXPECT_TRUE(tx.Make(from, to, 100));
 }
