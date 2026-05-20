896f9d3bad9907cd4e6914e2009170d516a6562d
boban4 <garik.melikov@bk.ru>
2026-05-20 16:05:26 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,4 +1,3 @@
-
 #include "Account.h"
 #include "Transaction.h"
 #include <gtest/gtest.h>
@@ -35,16 +34,20 @@ TEST(Transaction, MockTest) {
     Transaction tx;
     tx.set_fee(100);
 
-    EXPECT_CALL(from, Lock()).Times(1);
-    EXPECT_CALL(to, Lock()).Times(1);
-    
-    EXPECT_CALL(to, ChangeBalance(500)).Times(1);
-    
-    EXPECT_CALL(to, GetBalance()).WillOnce(Return(1000));
-    EXPECT_CALL(to, ChangeBalance(-600)).Times(1);
-    
-    EXPECT_CALL(from, Unlock()).Times(1);
-    EXPECT_CALL(to, Unlock()).Times(1);
+    {
+        testing::InSequence seq;
+
+        EXPECT_CALL(from, Lock()).Times(1);
+        EXPECT_CALL(to, Lock()).Times(1);
+        
+        EXPECT_CALL(to, ChangeBalance(500)).Times(1);
+        
+        EXPECT_CALL(to, GetBalance()).WillOnce(Return(1000));
+        EXPECT_CALL(to, ChangeBalance(-600)).Times(1);
+        
+        EXPECT_CALL(from, Unlock()).Times(1);
+        EXPECT_CALL(to, Unlock()).Times(1);
+    }
 
     EXPECT_TRUE(tx.Make(from, to, 500));
 }
